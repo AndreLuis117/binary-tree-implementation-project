1b90daf13c70c89d31a3c66a8aa02214a672f038
Elivelton Douglas Becker <38963615+SrYoshimaa@users.noreply.github.com>
2019-10-28 20:54:18 -0300

    Criar testes restantes

--- src/br/univille/estd/binarytree/IntegerBinaryTree.java
+++ src/br/univille/estd/binarytree/IntegerBinaryTree.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package br.univille.estd.binarytree;
 
 /**
--- src/br/univille/estd/binarytree/IntegerBinaryTreeImplTest.java
+++ src/br/univille/estd/binarytree/IntegerBinaryTreeImplTest.java
@@ -23,4 +23,39 @@ private IntegerBinaryTreeImpl impl;
 		impl.remove(10);
 		System.out.println();
 	}
+	@Test
+	public void testContains() {
+		impl.add(10);
+		impl.contains(10);
+		System.out.println();
+	}
+	@Test
+	public void testRemove() {
+		impl.add(10);
+		impl.remove(10);
+		System.out.println();
+	}
+	@Test
+	public void testisEmpty() {
+		impl.isEmpty();
+		System.out.println();
+	}
+	@Test
+	public void testSize() {
+		impl.add(10);
+		impl.size();
+		System.out.println();
+	}
+	@Test
+	public void testClear() {
+		impl.add(10);
+		impl.clear();
+		System.out.println();
+	}
+	@Test
+	public void testtoString() {
+		impl.add(10);
+		impl.toString();
+		System.out.println();
+	}
 }
